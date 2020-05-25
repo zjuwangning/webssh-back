93b61a36e2e91582f9a0d5b09f9b5a22d9706cc5
ReidWang <zjuwangning@zju.edu.cn>
2020-05-25 18:07:18 +0800

    first commit

---
+++ bin/server.js
@@ -0,0 +1,59 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var utf8 = require('utf8');
+var SSHClient = require('ssh2').Client;
+
+function createNewServer(machineConfig, socket) {
+    var ssh = new SSHClient();
+    let {msgId, ip, username, password} = machineConfig;
+    ssh.on('ready', function () {
+        console.log('1');
+        socket.emit(msgId, '\r\n***' + ip + ' SSH CONNECTION ESTABLISHED ***\r\n');
+        ssh.shell(function(err, stream) {
+            console.log('2');
+            if(err) {
+                console.log('3');
+                return socket.emit(msgId, '\r\n*** SSH SHELL ERROR: ' + err.message + ' ***\r\n');
+            }
+            console.log('4');
+            socket.on(msgId, function (data) {
+                stream.write(data);
+            });
+            stream.on('data', function (d) {
+                socket.emit(msgId, utf8.decode(d.toString('binary')));
+            }).on('close', function () {
+                ssh.end();
+            });
+        })
+    }).on('close', function () {
+        socket.emit(msgId, '\r\n*** SSH CONNECTION CLOSED ***\r\n');
+    }).on('error', function (err) {
+        console.log(err);
+        socket.emit(msgId, '\r\n*** SSH CONNECTION ERROR: ' + err.message + ' ***\r\n');
+    }).connect({
+        host: ip,
+        port: 22,
+        username: username,
+        password: password
+    }, function () {
+        console.log('5');
+        socket.emit(msgId, '\r\n*** SSH CONNECTION SUCCEED: ' + ' ***\r\n');
+    });
+}
+
+
+io.on('connection', function(socket) {
+    socket.on('createNewServer', function(machineConfig) {//新建一个ssh连接
+        console.log("createNewServer")
+        createNewServer(machineConfig, socket);
+    })
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+})
+
+http.listen(8000, function() {
+    console.log('listening on * 8000');
+})
